1d3b0c5194dbbb0df0717dc261a930fc725958ca
cbrit0 <cristobal.brito.p@gmail.com>
2025-01-24 14:47:03 -0300

    exercise 1.14

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -34,15 +34,24 @@ const App = () => {
     setVotes(newVotes)
   }
 
+  const maxVotes = Math.max(...votes);
+  const mostVotedIndex = votes.indexOf(maxVotes);
+
   return (
-    <div>
-      <p>{anecdotes[selected]}</p>
-      <p>has {votes[selected]} votes</p>
+    <>
       <div>
+        <h1>Anecdote of the day</h1>
+        <p>{anecdotes[selected]}</p>
+        <p>has {votes[selected]} votes</p>
         <Button onClick={handleVote} text="vote" />
         <Button onClick={handleClick} text="next anecdote" />
       </div>
-    </div>
+      <div>
+        <h1>Anecdote with most votes</h1>
+        <p>{anecdotes[mostVotedIndex]}</p>
+        <p>has {maxVotes} votes</p>
+      </div>
+    </>
   )
 }
 
