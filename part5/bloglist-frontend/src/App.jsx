e5c309d965c3a9e9477a3aa6571e118264b04aeb
cbrit0 <cristobal.brito.p@gmail.com>
2025-02-04 17:22:12 -0300

    exercise 5.3

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -3,12 +3,17 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import LoginForm from './components/LoginForm'
+import BlogForm from './components/BlogForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+
+  const [author, setAuthor] = useState('')
+  const [title, setTitle] = useState('')
+  const [url, setUrl] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -52,6 +57,18 @@ const App = () => {
     setUser(null)
   }
 
+  const handleCreate = () => {
+    blogService
+      .create({ author,title,url })
+      .then(response => {
+        setBlogs(blogs.concat(response))
+        setAuthor('')
+        setTitle('')
+        setUrl('')
+      })
+      .catch(error => console.log(error))
+  }
+
   return (
     <div>
       {user === null ? (
@@ -69,6 +86,16 @@ const App = () => {
         <div>
           <h2>blogs</h2>
           <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
+          <h2>create new</h2>
+          <BlogForm 
+            author={author}
+            title={title}
+            url={url}
+            setAuthor={setAuthor}
+            setTitle={setTitle}
+            setUrl={setUrl}
+            handleCreate={handleCreate}
+          />
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
           )}
