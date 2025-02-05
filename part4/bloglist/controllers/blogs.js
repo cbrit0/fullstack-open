8015f0e579ceff4a823a591370bbc3376d8b2824
cbrit0 <cristobal.brito.p@gmail.com>
2025-02-05 20:35:32 -0300

    minor fix

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -18,6 +18,7 @@ blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
   })
 
   const savedBlog = await blog.save()
+  await savedBlog.populate('user', { username: 1, name: 1 })
   user.blogs = user.blogs.concat(savedBlog._id)
   await user.save()
 
