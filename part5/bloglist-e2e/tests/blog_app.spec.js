880933c5a58c4f2e3eb0d4e8615a61c5c66edc07
cbrit0 <cristobal.brito.p@gmail.com>
2025-02-05 15:29:10 -0300

    exercise 5.18

--- part5/bloglist-e2e/tests/blog_app.spec.js
+++ part5/bloglist-e2e/tests/blog_app.spec.js
@@ -1,8 +1,17 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:5173')
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/tests/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'Matti Luukkainen',
+        username: 'mluukkai',
+        password: 'salainen'
+      }
+    })
+
+    await page.goto('/')
   })
 
   test('Login form is shown', async ({ page }) => {
@@ -12,6 +21,27 @@ describe('Blog app', () => {
     await expect(page.locator('input[name="Username"]')).toBeVisible()
     await expect(page.locator('input[name="Password"]')).toBeVisible()
 
-    await expect(page.locator('button', { hasText: 'Login' })).toBeVisible()
+    await expect(page.locator('button', { name: 'login' })).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByTestId('username').fill('mluukkai')
+      await page.getByTestId('password').fill('salainen')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByTestId('username').fill('mluukkai')
+      await page.getByTestId('password').fill('wrong')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      const notificationDiv = await page.locator('.notification')
+      await expect(notificationDiv).toContainText('wrong username or password')
+
+      await expect(page.getByText('Matti Luukkainen logged in')).not.toBeVisible()
+    })
   })
 })
