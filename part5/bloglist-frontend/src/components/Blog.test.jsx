dceb3c0dc8309e3a3f5f061445596965e4d0bb38
cbrit0 <cristobal.brito.p@gmail.com>
2025-02-05 00:37:35 -0300

    exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
-import Notification from './Notification'
+import userEvent from '@testing-library/user-event'
 
-test('renders title and author, but not URL or likes by default', () => {
+describe('Blog component', () => {
   const blog = {
     title: 'Testing React Components',
     author: 'Test Author',
@@ -11,10 +11,28 @@ test('renders title and author, but not URL or likes by default', () => {
     user: { name: 'Test User', username: 'testuser' },
   }
 
-  render(<Blog blog={blog} user={{ username: 'testuser' }} likeBlog={(blog) => {}} deleteBlog={(blog) => {}} />)
+  test('renders title and author, but not URL or likes by default', () => {
+    render(<Blog blog={blog} user={{ username: 'testuser' }} likeBlog={(blog) => {}} deleteBlog={(blog) => {}} />)
 
-  expect(screen.getByText('Testing React Components Test Author')).toBeDefined()
+    expect(screen.getByText('Testing React Components Test Author')).toBeDefined()
 
-  expect(screen.queryByText('https://test.com')).toBeNull()
-  expect(screen.queryByText('Likes: 5')).toBeNull()
+    expect(screen.queryByText('https://test.com')).toBeNull()
+    expect(screen.queryByText('Likes: 5')).toBeNull()
+  })
+
+  test('shows URL and number of likes when the "View" button is clicked', async () => {
+    const mockHandler = vi.fn()
+
+    render(<Blog blog={blog} user={{ username: 'testuser' }} likeBlog={(blog) => {}} deleteBlog={(blog) => {}} />)
+
+    expect(screen.queryByText('https://test.com')).toBeNull()
+    expect(screen.queryByText('likes 5')).toBeNull()
+
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    expect(screen.queryByText('https://test.com')).toBeDefined()
+    expect(screen.queryByText('likes 5')).toBeDefined()
+  })
 })
